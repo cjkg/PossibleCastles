822d650b9778b09bd91af5471db6383b0146d60f
Chris Gibilisco <cjgibilisco@gmail.com>
2023-11-24 22:36:24 -0600

    starting on Jamis's maze algorithms

--- PossibleCastles/Map/Cell.cs
+++ PossibleCastles/Map/Cell.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 namespace PossibleCastles;
 
 public class Cell
+// Thanks to Jamis Buck for the idea behind this class
 {
     public Cell(int row, int column)
     {
@@ -13,6 +14,11 @@ public class Cell
 
     public readonly int Row;
     public readonly int Column;
+    public Cell? North { get; set; }
+    public Cell? East { get; set; }
+    public Cell? South { get; set; }
+    public Cell? West { get; set; }
+    
     private Dictionary<Cell, bool> Links { get; set; }
 
     public Cell Link(Cell cell, bool bidi = true)
@@ -28,6 +34,24 @@ public class Cell
         if (bidi) cell.Unlink(this, false);
         return this;
     }
-    
-    
+
+    public List<Cell> linksKeys()
+    {
+        return Links.Keys.ToList();
+    }
+
+    public bool IsLinked(Cell cell)
+    {
+        return linksKeys().Contains(cell);
+    }
+
+    public List<Cell> Neighbors()
+    {
+        List<Cell> neighbors = new();
+        if (North != null) neighbors.Add(North);
+        if (East != null) neighbors.Add(East);
+        if (South != null) neighbors.Add(South);
+        if (West != null) neighbors.Add(West);
+        return neighbors;
+    }
 }
--- PossibleCastles/Map/Grid.cs
+++ PossibleCastles/Map/Grid.cs
@@ -1,6 +1,39 @@
 namespace PossibleCastles;
 
 public class Grid
+// Thanks to Jamis Buck for the idea behind this class
 {
+    public Grid(int rows, int columns)
+    {
+        Rows = rows;
+        Columns = columns;
+        Grid = PrepareGrid();
+        ConfigureCells();
+    }
     
+    public int Rows { get; set; }
+    public int Columns { get; set; }
+
+    public List<List<Cell>> PrepareGrid()
+    {
+        List<List<Cell>> Grid = new();
+        Cell cell;
+        for (int i = 0; i < Rows; i++)
+        {
+            List<Cell> row = new();
+            for (int j = 0; j < Rows; j++)
+            {
+                cell = new Cell(i, j);
+                row.Add(cell);
+            }
+            Grid.Add(row);
+        }
+
+        return Grid;
+    }
+
+    public void ConfigureCells()
+    {
+        
+    }
 }
